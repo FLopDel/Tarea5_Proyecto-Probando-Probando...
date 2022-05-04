20c0491506aec5013dfc533d8abb12e2b0b42d90
FLopDel <99647790+FLopDel@users.noreply.github.com>
2022-05-04 12:47:45 +0200

    Ejercicio Documentado

--- busquedabinaria.java
+++ busquedabinaria.java
@@ -1,10 +1,22 @@
 package busquedabinaria;
-
+/**
+ * @author Francisco Lopez
+ * @version 1.1
+ */
 public class busquedabinaria {
+	/**
+	 * Clase basada en busqueda de elementos
+	 */
 
 	public static int contador;
-
+	
+	/**
+	 * @param args argumento
+	 * @throws Exception Se produce si encuentra un error interno
+	 */
+	
 	public static void main(String[] args) throws Exception {
+		//TODO Auto-generated method stub
 		int[] v = new int[40000000];
 		for(int j=0;j<v.length; j++) {
 			v[j] = j;
@@ -12,8 +24,14 @@ public class busquedabinaria {
 		int busca = 3447;
 		System.out.print("Valor: " + buscab(v,busca) + " encontrado en el intento " + contador);
 	}
-
+	
+	/**
+	 * @param vector primer_numero
+	 * @param valorabuscar segundo_numero
+	 * @return devuelve un entero
+	 */
 	public static int buscab(int[] vector, int valorabuscar) {
+		//TODO Auto-generated method stub
 		int mitad, inferior = 0;
 		int superior = vector.length - 1;
 		contador = 0;
